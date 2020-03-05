c8cf12758d4c43d1d9d831b532ad79ddd48360d2
sunatomo <tomokisunaga@yahoo.co.jp>
2020-03-05 13:16:47 +0900

    Update hotel.js

--- hotel.js
+++ hotel.js
@@ -1,1 +1,22 @@
-alert('ok');
+f=e=>{
+ D.innerText='serching';
+ fetch('/gs.php?keyword='+encodeURI(e)).then(e=>e.text()).then(e=>{
+  A=e;
+  D.innerHTML=e.match(/<a.*title.*<\/a>/g).sort().join('<br>');
+  B=D.querySelectorAll('a');
+  i=0;
+  for(a of B) {
+   a.I=i++;
+   a.onclick=q=>{
+    fetch(q.target.href).then(e=>e.text()).then(e=>{
+     V.src=e.match(/file:.*(http[^']*)/)[1];
+     V.onpause=()=>{
+	  if(V.currentTime>V.duration-300) B[q.target.I+1].click();
+     };
+    });
+    return false;
+   }
+  }
+ });
+};
+document.body.innerHTML = '<div style="float: left;" style="float: left;"><input onchange=f(value)><div id=D></div></div><video id=V controls autoplay>';
